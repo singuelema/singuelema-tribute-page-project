687a6553fce7f9f4d6808cb73b2e71387e65bf0d
singuelema <49936646+singuelema@users.noreply.github.com>
2020-01-30 19:42:56 +0800

    Add files via upload

---
+++ Question.java
@@ -0,0 +1,8 @@
+public class Question {
+    String answer;
+    String prompt;
+    public Question(String prompt,String answer){
+        this.answer= answer;
+        this.prompt = prompt;
+    }
+}
