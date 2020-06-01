27af2b62d8ec53f48263d6322240d2d290cecdfe
singuelema <49936646+singuelema@users.noreply.github.com>
2020-06-01 13:53:12 +0800

    Delete Question.java

--- Question.java
+++
@@ -1,8 +0,0 @@
-public class Question {
-    String answer;
-    String prompt;
-    public Question(String prompt,String answer){
-        this.answer= answer;
-        this.prompt = prompt;
-    }
-}
